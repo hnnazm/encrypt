ad583ec0fabd7d0c463623e51763ffdef83b3adb
Hannan Azmi <mhannan143@gmail.com>
2021-08-13 01:09:18 +0800

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import '../css/Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+      <p>Website created by <strong>Hannan Azmi</strong></p>
+    </footer>
+  )
+}
+
+export default Footer
