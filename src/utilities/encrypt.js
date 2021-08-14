68174d00d2ae2d49bfa533dbfd3cdafd0a689725
Hannan Azmi <mhannan143@gmail.com>
2021-08-14 10:46:06 +0800

    Code cleanup and comment

--- src/utilities/encrypt.js
+++ src/utilities/encrypt.js
@@ -1,41 +1,30 @@
+const ALBHABET_NUM = 26   // alphabet letters amount
 const VOWELS = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
-// export default function encrypt(originalText) {
-//   const letters = originalText.split('')
-//   // const randomKey = generateRandom(MIN, MAX)
-//   const encryptedLetter = []
-// 
-//   letters.forEach((letter) => {
-//     if (VOWELS.includes(letter))
-//       encryptedLetter.push(String.fromCharCode(letter.charCodeAt() + 2))
-//     else if (letter == ' ') encryptedLetter.push(' ')
-//     else {
-//       if (letter == 'z') encryptedLetter.push('a')
-//       else if (letter == 'Z') encryptedLetter.push('A')
-//       else encryptedLetter.push(String.fromCharCode(letter.charCodeAt() + 6))
-//     }
-//   })
-// 
-//   return encryptedLetter.join('')
-// }
+// Character Code reference for shifting limitation
+// letter 'A' start on 65
+// letter 'Z' end on 90
+// letter 'a' start on 97
+// letter 'z' end on 122
 
 export function encrypt(originalText, randomKey) {
-  var n = 26; // alphabet letters amount
+  return originalText
+    .split('')
+    .map(letter => {
+      if (letter.match(/[a-z]/i)) {   // match regex case insensitive
+        let characterCode = letter.charCodeAt()
+        let shift = characterCode >= 65 && characterCode <= 90 ? 65 :
+          characterCode >= 97 && characterCode <= 122 ? 97 : 0
 
-  if (randomKey[1] < 0) {
-    return encrypt(originalText, randomKey[1] + n);
-  }
-
-  return originalText.split('')
-    .map(function (letter) {
-      if (letter.match(/[a-z]/i)) {
-        var code = letter.charCodeAt();
-        var shift = code >= 65 && code <= 90 ? 65 : code >= 97 && code <= 122 ? 97 : 0;
         if (VOWELS.includes(letter))
-          return String.fromCharCode(((code - shift + parseInt(randomKey[0])) % n) + shift);
+          return String.fromCharCode(((characterCode - shift + parseInt(randomKey[0]))
+            % ALBHABET_NUM) + shift)
         else
-          return String.fromCharCode(((code - shift + parseInt(randomKey[1])) % n) + shift);
+          return String.fromCharCode(((characterCode - shift + parseInt(randomKey[1]))
+            % ALBHABET_NUM) + shift)
       }
-      return letter;
-    }).join('');
+
+      return letter   // if the letter is non-character, just return as it is
+    })
+    .join('')
 }
--- src/utilities/random.js
+++ src/utilities/random.js
@@ -1,4 +1,5 @@
 // MAX is exclusive so need to be increased by one
+// return an array of two number for the encryption algorithm
 export function generateRandom(MIN, MAX) {
-  return (Math.floor(Math.random() * (MAX - MIN)) + MIN).toString().split('')
+  return (Math.floor(Math.random() * MAX) + MIN).toString().split('')
 }
